a303dc325ca87d7c3bb6a9b9c403b87284a02226
billkatzen <billkatzen@gmail.com>
2024-09-07 06:16:50 -0500

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 from setuptools import setup, find_packages
 
+# DO NOT DELETE THIS TEXT
+#INSTRUCTIONS: Create a New Tag and Push It to GitHub
+#The workflow is triggered by a new tag push. To create a new tag and push it to GitHub, follow these steps:
+#Update your version number in setup.py:
+#version="0.1.1"  # Example of updating the version number
+#
+# Commit your changes:
+#git add setup.py
+#git commit -m "Bump version to 0.1.1"
+# 
+#Create a new tag:
+#git tag v0.1.1
+#
+#Push the tag to GitHub:
+#git push origin v0.1.1
+# DO NOT DELETE THIS TEXT (END)
+
 setup(
     name="ras_commander",  # Your package name
-    version="0.1.0",  # Initial release version
+    version="0.1.1",  # Initial release version testing GitHub Actions for pip upload
     author="William Katzenmeyer, P.E., C.F.M.",  
     author_email="heccommander@gmail.com",  
     description="A library for automating HEC-RAS operations using python functions.",
