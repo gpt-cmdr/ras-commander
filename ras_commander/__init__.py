fce808a18926261075219aefeca12b2610142124
billkatzen <billkatzen@gmail.com>
2024-09-07 20:34:42 -0500

    update github actions
    update github actions

--- ras_commander/__init__.py
+++ ras_commander/__init__.py
@@ -1,24 +1,7 @@
-"""
-ras_commander
-A library for automating HEC-RAS operations.
-"""
-from .project_init import init_ras_project
-from .file_operations import FileOperations
-from .project_management import ProjectManager
-from .plan_operations import PlanOperations
-from .geometry_operations import GeometryOperations
-from .unsteady_operations import UnsteadyOperations
-from .execution import RasExecutor
-from .utilities import Utilities
+from importlib.metadata import version, PackageNotFoundError
 
-__all__ = [
-    'init_ras_project',
-    'FileOperations',
-    'ProjectManager',
-    'PlanOperations',
-    'GeometryOperations',
-    'UnsteadyOperations',
-    'RasExecutor',
-    'Utilities'
-]
-__version__ = "0.1.0"
+try:
+    __version__ = version("ras_commander")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
