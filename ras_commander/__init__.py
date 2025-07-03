3e0ae395026752c04d93cfde2c00f29e67f2ff3d
William Mark Katzenmeyer, P.E., C.F.M. <125500944+billk-FM@users.noreply.github.com>
2025-07-03 12:10:27 -0400

    Bug Fix: Unsteady Tables Not Including Decimals
    Fixed the write_table_to_file function to include 2 decimal places instead of 0.

--- ras_commander/__init__.py
+++ ras_commander/__init__.py
@@ -10,7 +10,7 @@ try:
     __version__ = version("ras-commander")
 except PackageNotFoundError:
     # package is not installed
-    __version__ = "0.79.0"
+    __version__ = "0.79.1"
 
 # Set up logging
 setup_logging()
